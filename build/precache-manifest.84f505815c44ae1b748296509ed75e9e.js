d6f220b794751821f68e29311097b196d60ebc13
sparsh229 <sparsh.gupta229@gmail.com>
2021-05-24 04:35:45 +0530

    Build packs

---
+++ build/precache-manifest.84f505815c44ae1b748296509ed75e9e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402896aef270014d29267ef9360b3def",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbbed2bf7928a43be96",
+    "url": "/static/js/2.fabae278.chunk.js"
+  },
+  {
+    "revision": "e704b783db998d08b093",
+    "url": "/static/js/main.b9704480.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
